cc299605bb83fe3bd2c280ed514845d0066bb091
Arjun Sah <arjunsah@sussol.net>
2020-05-15 15:57:31 +0545

    Handling Navigation styles and addes some data to shared

--- src/components/Main.js
+++ src/components/Main.js
@@ -11,7 +11,15 @@ const Drawer = createDrawerNavigator();
 
 const MenuStackNavigator = () => {
     return(
-        <Stack.Navigator initialRouteName='Menu'>
+        <Stack.Navigator initialRouteName='Menu' screenOptions={{
+            headerStyle: {
+                backgroundColor: '#512DA8',
+            },
+            headerTintColor: '#fff',
+            headerTitleStyle: {
+                fontWeight: 'bold',
+            },
+        }}>
             <Stack.Screen name='Menu' component={Menu} options={{title: 'Menu'}} />
             <Stack.Screen name='DishDetail' component={DishDetail} options={{title: 'Dish Details'}} />
         </Stack.Navigator>
@@ -20,9 +28,9 @@ const MenuStackNavigator = () => {
 class Main extends Component {
     render() {
         return(
-            <Drawer.Navigator>
-                <Drawer.Screen name='Home' component={Home} options={{title: 'Home'}} />
-                <Drawer.Screen name='MenuStackNavigator' component={MenuStackNavigator} options={{title: 'Menu'}} />
+            <Drawer.Navigator initialRouteName='Home'>
+                <Drawer.Screen name='Home' component={Home} options={{drawerLabel: 'Home'}} />
+                <Drawer.Screen name='MenuStackNavigator' component={MenuStackNavigator} options={{drawerLabel: 'Menu'}} />
             </Drawer.Navigator>
         )
     }
