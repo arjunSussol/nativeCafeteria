0aff5568bed73c7ba503c9949f433e97662a6977
Arjun Sah <arjunsah@sussol.net>
2020-05-15 09:50:43 +0545

    Introduced React Native Drawer Navigation

--- src/components/Main.js
+++ src/components/Main.js
@@ -1,18 +1,29 @@
 import React, { Component } from 'react';
 import { createStackNavigator } from '@react-navigation/stack';
+import { createDrawerNavigator } from '@react-navigation/drawer';
 
 import Menu from './Menu';
 import DishDetail from './DishDetail';
+import Home from './Home';
 
 const Stack = createStackNavigator();
+const Drawer = createDrawerNavigator();
 
+const MenuStackNavigator = () => {
+    return(
+        <Stack.Navigator initialRouteName='Menu'>
+            <Stack.Screen name='Menu' component={Menu} options={{title: 'Menu'}} />
+            <Stack.Screen name='DishDetail' component={DishDetail} options={{title: 'Dish Details'}} />
+        </Stack.Navigator>
+    )
+}
 class Main extends Component {
     render() {
         return(
-            <Stack.Navigator initialRouteName='Menu'>
-                <Stack.Screen name='Menu' component={Menu} options={{title: 'Menu'}} />
-                <Stack.Screen name='DishDetail' component={DishDetail} options={{title: 'Dish Details'}} />
-            </Stack.Navigator>
+            <Drawer.Navigator>
+                <Drawer.Screen name='Home' component={Home} options={{title: 'Home'}} />
+                <Drawer.Screen name='MenuStackNavigator' component={MenuStackNavigator} options={{title: 'Menu'}} />
+            </Drawer.Navigator>
         )
     }
 }
