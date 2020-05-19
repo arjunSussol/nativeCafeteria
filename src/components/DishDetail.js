75e7183f1b68bbffd3f1180db4984db3062cebc5
Arjun Sah <arjunsah@sussol.net>
2020-05-19 13:12:19 +0545

    Added comments to the Dishdetail using Hooks

--- src/components/DishDetail.js
+++ src/components/DishDetail.js
@@ -1,8 +1,9 @@
 import React, { useState } from 'react';
-import { Text, View } from 'react-native';
+import { Text, View, FlatList } from 'react-native';
 import { Card } from 'react-native-elements';
 
 import { DISHES } from '../shared/dishes';
+import { COMMENTS } from '../shared/comments';
 
 const RenderDish = props => {
     const dish = props.dish;
@@ -19,12 +20,42 @@ const RenderDish = props => {
         return <View></View>
     }
 }
+
+const RenderComments = props => {
+    const commentList = props.comment;
+
+    const renderCommentList = ({ item, index }) => {
+        return(
+            <View key={index} style={{margin: 10}}>
+                <Text style={{fontSize: 14}}>{item.comment}</Text>
+                <Text style={{fontSize: 12}}>{item.rating} Stars</Text>
+                <Text style={{fontSize: 12}}>{'-- ' + item.author + ', ' + item.date} </Text>
+            </View>
+        )
+    };
+
+    return(
+        <Card title='Comments'>
+            <FlatList
+                data={commentList}
+                renderItem={renderCommentList}
+                keyExtractor={item => item.id.toString()} 
+            />
+        </Card>
+    )
+}
+
 const DishDetail = ({ route }) => {
         const [dishes, setDishes] = useState(DISHES); // Hooks
+        const [comments, setComments] = useState(COMMENTS); // Hooks
+
         const { dishId } = route.params;
         const dishID = JSON.stringify(dishId);
         return(
-            <RenderDish dish={dishes[dishID]}/>
+            <View>
+            <RenderDish dish={dishes[dishID]} />
+            <RenderComments comment={comments.filter(comment => comment.dishId === dishId)} />
+            </View>
         )
 }
 
