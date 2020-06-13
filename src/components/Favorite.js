e1888b2131e36b3c595e2e740f14244b2b413fbd
Arjun Sah <arjunsah@sussol.net>
2020-06-13 20:28:52 +0545

    Introduced SwipeOut to Favorite

--- src/components/Favorite.js
+++ src/components/Favorite.js
@@ -1,10 +1,12 @@
 import React, { Component } from 'react';
-import { FlatList, View, Text } from 'react-native';
+import { StyleSheet, TouchableOpacity, View, Text, TouchableHighlight } from 'react-native';
 import { ListItem } from 'react-native-elements';
 import { connect } from 'react-redux';
+import { SwipeListView } from 'react-native-swipe-list-view';
 
 import { Loading } from './Loading';
 import { baseUrl } from '../shared/baseUrl';
+import { deleteFavorite } from '../redux/ActionCreators';
 
 const mapStateToProps = state => {
     return {
@@ -13,24 +15,58 @@ const mapStateToProps = state => {
     }
 }
 
+const mapDispatchToProps = dispatch => ({
+    deleteFavorite: (dishId) => dispatch(deleteFavorite(dishId))
+})
+
 class Favorite extends Component {
 
     render(){
         const { navigate } = this.props.navigation;
-        const { dishes, favorites } = this.props;
+        const { dishes, favorites, deleteFavorite } = this.props;
+
+        const closeRow = (rowMap, rowKey) => {
+            if (rowMap[rowKey]) {
+                rowMap[rowKey].closeRow();
+            }
+        };
+
+        const deleteRow = (rowMap, rowKey) => {
+            closeRow(rowMap, rowKey);
+            deleteFavorite(rowKey);
+        };
 
         const renderMenuItem = ({item, index}) => {
-            return(
-                <ListItem 
-                    key={index}
-                    title={item.name}
-                    subtitle={item.description}
-                    chevron={true}
-                    leftAvatar={{source: {uri: baseUrl + item.image}}}
-                    onPress={()=>navigate('DishDetail', {dishId: item.id})}
+            return(                
+                <TouchableHighlight>
+                    <ListItem 
+                        key={index}
+                        title={item.name}
+                        subtitle={item.description}
+                        chevron={true}
+                        leftAvatar={{source: {uri: baseUrl + item.image}}}
+                        onPress={()=>navigate('DishDetail', {dishId: item.id})}
                     />
+                </TouchableHighlight>
             )
         };
+
+        const renderHiddenItem = (data, rowMap) => (
+            <View style={styles.rowBack}>
+                <TouchableOpacity
+                    style={[styles.backRightBtn, styles.backRightBtnLeft]}
+                    onPress={() => closeRow(rowMap, data.item.id)}
+                >
+                    <Text style={styles.backTextWhite}>Close</Text>
+                </TouchableOpacity>
+                <TouchableOpacity
+                    style={[styles.backRightBtn, styles.backRightBtnRight]}
+                    onPress={() => deleteRow(rowMap, data.item.id)}
+                >
+                    <Text style={styles.backTextWhite}>Delete</Text>
+                </TouchableOpacity>
+            </View>
+        );
 
         if (dishes.isLoading) {
             return <Loading />
@@ -44,11 +80,16 @@ class Favorite extends Component {
         } 
         else {
             return(
-                <View>
-                    <FlatList 
+                <View style={styles.container}>
+                    <SwipeListView 
                         data={dishes.dishes.filter(dish => favorites.some(el => el === dish.id))}
                         renderItem={renderMenuItem}
+                        renderHiddenItem={renderHiddenItem}
                         keyExtractor={item => item.id.toString()}
+                        rightOpenValue={-150}
+                        previewRowKey={'0'}
+                        previewOpenValue={-40}
+                        previewOpenDelay={3000}
                         />
                 </View>
             )
@@ -56,4 +97,38 @@ class Favorite extends Component {
     }
 }
 
-export default connect(mapStateToProps)(Favorite);
+const styles = StyleSheet.create({
+    container: {
+        backgroundColor: 'white',
+        flex: 1,
+    },
+    backTextWhite: {
+        color: '#FFF',
+    },
+    rowBack: {
+        alignItems: 'center',
+        backgroundColor: '#DDD',
+        flex: 1,
+        flexDirection: 'row',
+        justifyContent: 'space-between',
+        paddingLeft: 15,
+    },
+    backRightBtn: {
+        alignItems: 'center',
+        bottom: 0,
+        justifyContent: 'center',
+        position: 'absolute',
+        top: 0,
+        width: 75,
+    },
+    backRightBtnLeft: {
+        backgroundColor: 'blue',
+        right: 75,
+    },
+    backRightBtnRight: {
+        backgroundColor: 'red',
+        right: 0,
+    },
+});
+
+export default connect(mapStateToProps, mapDispatchToProps)(Favorite);
--- src/shared/baseUrl.js
+++ src/shared/baseUrl.js
@@ -1,3 +1,3 @@
-export const baseUrl = 'http://192.168.1.100:3003/';
-// Run json-server @ json-server db.json -H 192.168.1.100 -p 3003
+export const baseUrl = 'http://192.168.1.105:3003/';
+// Run json-server @ json-server db.json -H 192.168.1.105 -p 3003
 // https://stackoverflow.com/questions/51026532/json-server-cannot-access-via-local-ip
